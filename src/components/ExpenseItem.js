0fdb3682163fa04cc4d7932c17954f1450edd624
Misha2007 <mishadrogovoz@gmail.com>
2024-11-15 17:04:36 +0200

    Wrap ExpenseItem component with Card component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
+import Card from "./Card";
 
 const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.data.date} />
       <div className="expense-item__description">
         <h2>{props.data.title}</h2>
         <div className="expense-item__price">{props.data.price}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
