062ccddcc3ab0561a69585b1efed44afb7f7b785
Misha2007 <mishadrogovoz@gmail.com>
2024-11-23 12:16:08 +0200

    Adding ExpenseFilter

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,11 +1,17 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem.jsx";
 import Card from "../UI/Card.jsx";
+import ExpensesFilter from "./ExpensesFilter.jsx";
 
 const Expenses = (props) => {
   console.log(props.expenses);
+  const saveExpenseDataHandler = (newYear) => {
+    console.log(`Year data in Expenses.js : ${newYear}`);
+    console.log();
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter onYearChange={saveExpenseDataHandler} />
       <ExpenseItem expenseData={props.expenses[0]} />
       <ExpenseItem expenseData={props.expenses[1]} />
     </Card>
